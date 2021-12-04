5c87708457787ee4bdbee9dbcf589113b5cfa236
ChanGeunPark <design795@naver.com>
2021-12-04 19:16:17 +0900

    Signed-off-by: ChanGeunPark <design795@naver.com>

--- web.js
+++ web.js
@@ -1,14 +1,20 @@
-const express = require('express')
-const app = express()
-const PORT = 8001
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const app = express();
+const PORT = 8001;
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.use(express.static('views'));//경로 설청해줘야함 그래야지 url,src활용가능
 app.get('/', (req, res) => {
   res.render('index')
-})
+});
+
 app.listen(PORT, () => {
     console.log(`server started on PORT ${PORT}`)
 })
-//testeess
-
 // web.js
